3da3b36c2ccc83a3b96f60aaed8c65893cd39c45
Andrey Shamis <lolnik@gmail.com>
2018-02-22 14:42:09 +0200

    Add parser
    Change-Id: I4b008848d39586f514e0467471369ca42b3c8e82
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Controller/LogBookUploaderController.php
+++ src/Controller/LogBookUploaderController.php
@@ -34,6 +34,17 @@ class LogBookUploaderController extends Controller
         //return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
     }
 
+    protected function clean_string($string) {
+        $s = trim($string);
+        //$s = iconv("UTF-8", "UTF-8//IGNORE", $s); // drop all non utf-8 characters
+
+        // this is some bad utf-8 byte sequence that makes mysql complain - control and formatting i think
+        $s = preg_replace('/(?>[\x00-\x1F]|\xC2[\x80-\x9F]|\xE2[\x80-\x8F]{2}|\xE2\x80[\xA4-\xA8]|\xE2\x81[\x9F-\xAF])/', ' ', $s);
+
+        //$s = preg_replace('/\s+/', ' ', $s); // reduce all multiple whitespace to a single space
+
+        return $s;
+    }
     /**
      * Creates a new verdict entity.
      *
@@ -51,10 +62,10 @@ class LogBookUploaderController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             // $file stores the uploaded PDF file
-            /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            /** @var UploadedFile $file */
             $file = $obj->getLogFile();
 
-            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
+            $fileName = $this->generateUniqueFileName(). '_' . $file->getClientOriginalName(). '.'.$file->guessExtension();
             // moves the file to the directory where brochures are stored
 //            $file->move(
 //                $this->getParameter('brochures_directory'),
@@ -63,12 +74,16 @@ class LogBookUploaderController extends Controller
             $obj->addMessage("New file name is " . $fileName);
             $obj->addMessage("File ext "  .$file->guessExtension());
             $copy_info = $file->move("../uploads/", $fileName);
+
             $obj->addMessage("File copy info "  . $copy_info);
+            $obj->setLogFile($fileName);
+            $obj->file_info = $file;
+            $obj->data = $this->parseFile($copy_info);
             //$file->move("uploads/", $fileName);
 
             // updates the 'brochure' property to store the PDF file name
             // instead of its contents
-            $obj->setLogFile($fileName);
+
 //
 //            $em = $this->getDoctrine()->getManager();
 //            $em->persist($obj);
@@ -85,6 +100,66 @@ class LogBookUploaderController extends Controller
         ));
     }
 
+    /**
+     * @param  $file
+     */
+    public function parseFile($file){
+
+        $MIN_STR_LEN = 10;
+
+        $temp_arr = "";
+        $ret_data =array();
+        $file_data               = file_get_contents($file , FILE_USE_INCLUDE_PATH);
+        $temp_arr = preg_split("/\\r\\n|\\r|\\n/", $file_data);
+
+        $last_good_key = -1;
+        foreach ($temp_arr as $key => &$value){
+            //preg_match_all('/(\d{4,4}\-\d+\-\d+)\s+(\d+\:\d+:\d+)\s+([\d\w]+)\s+(\w+)\:\s+(.*)/', $value,$oneLine);
+            //preg_match_all('/(\d{2,}.*)\s*\|\s*(.*)/', $value,$oneLine);
+            //preg_match_all('/(\d{2,}.*\d{2,})\s*(\w+)\s*\|\s*(.*)/', $value,$oneLine);
+            if(strlen($value) < $MIN_STR_LEN){
+                continue;
+            }
+            preg_match_all('/(\d{2,}.*\d{1,1})\s*([A-Z]+)\s*\|\s*(.*)/', $value,$oneLine);
+
+            if (count($oneLine[2]) > 0){
+                $last_good_key = $key;
+                //echo $oneLine[1][0] . " ___ " .  $oneLine[2][0] . " ___ " .  $oneLine[3][0] . "<br/>";
+            }
+            else{
+                $temp_arr[$last_good_key] = $temp_arr[$last_good_key] . "\n" . $value;
+//                echo count($oneLine[0]) .  " :<pre>";
+//                print_r($oneLine);
+//                echo "</pre><br/>";
+
+                //echo "Update by value $value to index  $last_good_key , new value is " .  $temp_arr[$last_good_key] ."  <br/>";
+                $value = "";
+            }
+        }
+
+        $counter=0;
+        foreach ($temp_arr as $key => $value){
+            if(strlen($value) < $MIN_STR_LEN){
+                continue;
+            }
+            preg_match_all('/(\d{2,}.*\d{1,1})\s*([A-Z]+)\s*\|\s*(.*)/s', $value,$oneLine);
+
+            if (count($oneLine[2]) > 0){
+
+                //echo $oneLine[1][0] . " ___ " .  $oneLine[2][0] . " ___ " .  $oneLine[3][0] . "<br/>";
+                $ret_data[$counter]['time'] = $this->clean_string($oneLine[1][0]);
+                $ret_data[$counter]['debugLevel'] = $this->clean_string($oneLine[2][0]);
+                $ret_data[$counter]['msg'] = $this->clean_string($oneLine[3][0]);
+                $counter++;
+            }
+            else{
+                echo count($oneLine) .  " $value:<pre>";
+                print_r($oneLine);
+                echo "</pre><br/>";
+            }
+        }
+        return $ret_data;
+    }
 
 //    /**
 //     * Show upload file info.

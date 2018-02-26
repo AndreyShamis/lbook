2faa862b8d51fcc2c51cc433d0f899a48dff0347
Andrey Shamis <lolnik@gmail.com>
2018-02-26 13:09:08 +0200

    Add target Entity
    Change-Id: I371740b67f7c22b9a1001e5b92c7e1c14cc1b6e8
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ src/Entity/LogBookTarget.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogBookTargetRepository")
+ * @ORM\Table(name="lbook_targets", uniqueConstraints={@ORM\UniqueConstraint(name="uniq_name", columns={"name"})})
+ * @UniqueEntity("name", message="Target with this name already exist")
+ */
+class LogBookTarget
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     */
+    protected $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, options={"default"=""})
+     */
+    protected $name = "";
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_ip", type="boolean", options={"default"="0"})
+     */
+    protected $isIp = false;
+
+    /**
+     * @return bool
+     */
+    public function isIp(): bool
+    {
+        return $this->isIp;
+    }
+
+    /**
+     * @param bool $isIp
+     */
+    public function setIsIp(bool $isIp): void
+    {
+        $this->isIp = $isIp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+        $this->setIsIp(ip2long($this->name) !== false);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+}
---
+++ src/Repository/LogBookTargetRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LogBookTarget;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class LogBookTargetRepository extends ServiceEntityRepository
+{
+    /**
+     * @var array Keep hashed entity
+     */
+    protected static $_hashedData = array();
+
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, LogBookTarget::class);
+    }
+    
+    /**
+     * @param array $criteria
+     * @return LogBookTarget
+     */
+    public function findOneOrCreate(array $criteria)
+    {
+        $add_hash = true;
+        if(isset(self::$_hashedData[$criteria['name']])){
+            $entity = self::$_hashedData[$criteria['name']];
+            $add_hash = false;
+        }
+        else{
+            $entity = $this->findOneBy($criteria);
+        }
+
+        if (null === $entity) {
+            $entity = new LogBookTarget();
+            $entity->setName($criteria['name']);
+            $this->_em->persist($entity);
+            $this->_em->flush($entity);
+        }
+        if($add_hash) {
+            self::$_hashedData[$criteria['name']] = $entity;
+        }
+
+        return $entity;
+    }
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->where('l.something = :value')->setParameter('value', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}

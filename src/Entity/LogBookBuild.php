f5709925627f24cc6e3b5405c0136cdf934be9f0
Andrey Shamis <lolnik@gmail.com>
2018-02-26 11:55:27 +0200

    Add Cycle Build
    Change-Id: Ief58d4e1255d84fc41aa4f3c9f39870ec0ad77df
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ src/Entity/LogBookBuild.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogBookBuildRepository")
+ * @ORM\Table(name="lbook_build", uniqueConstraints={@ORM\UniqueConstraint(name="uniq_name", columns={"name"})})
+ * @UniqueEntity("name", message="Build name already exist")
+ */
+class LogBookBuild
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     */
+    protected $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, options={"default"=""})
+     */
+    protected $name = "";
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+}
---
+++ src/Repository/LogBookBuildRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LogBookBuild;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class LogBookBuildRepository extends ServiceEntityRepository
+{
+    /**
+     * @var array Keep hashed entity
+     */
+    protected static $_hashedData = array();
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, LogBookBuild::class);
+    }
+
+    /**
+     * @param array $criteria
+     * @return LogBookBuild
+     */
+    public function findOneOrCreate(array $criteria)
+    {
+        $criteria['name'] = strtoupper($criteria['name']);
+        $add_hash = true;
+        if(isset(self::$_hashedData[$criteria['name']])){
+            $entity = self::$_hashedData[$criteria['name']];
+            $add_hash = false;
+        }
+        else{
+            $entity = $this->findOneBy($criteria);
+        }
+
+        if (null === $entity) {
+            $entity = new LogBookBuild();
+            $entity->setName($criteria['name']);
+            $this->_em->persist($entity);
+            $this->_em->flush($entity);
+        }
+        if($add_hash) {
+            self::$_hashedData[$criteria['name']] = $entity;
+        }
+
+        return $entity;
+    }
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->where('l.something = :value')->setParameter('value', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}

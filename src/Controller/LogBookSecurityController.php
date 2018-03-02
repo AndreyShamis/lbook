3b1111eb02747eda817705dd0b499b238301d88a
Andrey Shamis <lolnik@gmail.com>
2018-03-02 11:48:38 +0200

    Login logout
    Change-Id: I5f543a9af39b08826e57fe17ef1acf1c145c3415
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ src/Controller/LogBookSecurityController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\LogBookUser;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class LogBookSecurityController extends Controller
+{
+    /**
+     * @Route("/logout", name="logout")
+     * @param Request $request
+     */
+    public function logout(Request $request){
+
+    }
+
+    /**
+     * @Route("/login", name="login")
+     * @param Request $request
+     * @param AuthenticationUtils $authUtils
+     * @return Response
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user_name = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        // Retrieve the security encoder of symfony
+
+        $user_manager = $this->getDoctrine()->getManager()->getRepository("App:LogBookUser");
+        /** @var LogBookUser $user */
+        $user = $user_manager->loadUserByUsername($user_name);
+
+        if($user !== null){
+            #$user = /*The user needs to be registered */;#
+            // Example of how to obtain an user:
+            /// Start verification
+            //$encoder = $factory->getEncoder($user);
+            $encoded_pass = $passwordEncoder->encodePassword($user, $password);
+            $salt = $user->getSalt();
+            if(!$passwordEncoder->isPasswordValid($user, $password)){
+            //if(!$encoder->isPasswordValid($user->getPassword(), $password, $salt)) {
+                return new Response(
+                    'Username or Password not valid.',
+                    Response::HTTP_UNAUTHORIZED,
+                    array('Content-type' => 'application/json')
+                );
+            }
+
+            // The password matches ! then proceed to set the user in session
+
+            //Handle getting or creating the user entity likely with a posted form
+            // The third parameter "main" can change according to the name of your firewall in security.yml
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->get('security.token_storage')->setToken($token);
+
+            // If the firewall name is not main, then the set value would be instead:
+            // $this->get('session')->set('_security_XXXFIREWALLNAMEXXX', serialize($token));
+            $this->get('session')->set('_security_main', serialize($token));
+
+            // Fire the login event manually
+            $event = new InteractiveLoginEvent($request, $token);
+            $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+            return $this->render('lbook/default/index.html.twig', array(
+            ));
+
+        }
+        else{
+            // get the login error if there is one
+            $error = $authUtils->getLastAuthenticationError();
+
+            // last username entered by the user
+            $lastUsername = $authUtils->getLastUsername();
+
+            return $this->render('lbook/login/login.html.twig', array(
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            ));
+        }
+//
+//        $user = $this->getDoctrine()->getManager()->getRepository("App:LogBookUser")->loadUserByUsername($user_name);
+//
+//        if($user !== null){
+//            //Handle getting or creating the user entity likely with a posted form
+//            // The third parameter "main" can change according to the name of your firewall in security.yml
+//            $token = new UsernamePasswordToken($user, $password, 'main', $user->getRoles());
+//
+//            $this->get('security.token_storage')->setToken($token);
+//
+//            // If the firewall name is not main, then the set value would be instead:
+//            // $this->get('session')->set('_security_XXXFIREWALLNAMEXXX', serialize($token));
+//            $this->get('session')->set('_security_main', serialize($token));
+//
+//            // Fire the login event manually
+//            $event = new InteractiveLoginEvent($request, $token);
+//            $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+//
+//            /*
+//             * Now the user is authenticated !!!!
+//             * Do what you need to do now, like render a view, redirect to route etc.
+//             */
+//            return $this->render('lbook/default/index.html.twig', array(
+//            ));
+//
+//        }
+//        else{
+//            // get the login error if there is one
+//            $error = $authUtils->getLastAuthenticationError();
+//
+//            // last username entered by the user
+//            $lastUsername = $authUtils->getLastUsername();
+//
+//            return $this->render('lbook/login/login.html.twig', array(
+//                'last_username' => $lastUsername,
+//                'error'         => $error,
+//            ));
+//        }
+
+
+    }
+
+//    /**
+//     * @Route("/log/book/security", name="log_book_security")
+//     */
+//    public function index()
+//    {
+//        // replace this line with your own code!
+//        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+//    }
+}
--- src/Entity/LogBookUser.php
+++ src/Entity/LogBookUser.php
@@ -4,14 +4,16 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LogBookUserRepository")
- * @ORM\Table(name="lbook_users", uniqueConstraints={@ORM\UniqueConstraint(name="uniq_name", columns={"name"})})
- * @UniqueEntity("name", message="This User Name already exist")
+ * @ORM\Table(name="lbook_users", uniqueConstraints={@ORM\UniqueConstraint(name="uniq_name", columns={"username"})})
+ * @UniqueEntity("username", message="This User Name already exist")
  */
-class LogBookUser
+class LogBookUser implements AdvancedUserInterface, \Serializable
 {
     /**
      * @ORM\Id
@@ -23,10 +25,21 @@ class LogBookUser
     /**
      * @var string
      *
-     * @ORM\Column(name="name", type="string", length=255)
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
      * @Assert\NotBlank(message="User Name cannot ne empty")
      */
-    protected $name = "";
+    protected $username = "";
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
 
     /**
      * @var string
@@ -35,6 +48,45 @@ class LogBookUser
      * @Assert\Email()
      */
     protected $email = "";
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        // may not be needed, see section on salt below
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
 
     /**
      * @return string
@@ -68,24 +120,185 @@ class LogBookUser
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
     public function __toString(): string
     {
-        return $this->getName();
+        return $this->getUsername();
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+        return array('ROLE_USER');
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+        // TODO: Implement getPassword() method.
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        $this->salt = md5( uniqid('', true));
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     * @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     * @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {
+        return $this->isActive;
     }
 }

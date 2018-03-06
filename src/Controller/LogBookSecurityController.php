b893aa5e98c7d4e1b27c0a6943fe94d1af020fec
Andrey Shamis <lolnik@gmail.com>
2018-03-06 14:28:43 +0200

    Add function for LDAP auth
    Change-Id: Ie277531cdb6306a35bdaec6c9eeb0cb7ef63e2eb
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Controller/LogBookSecurityController.php
+++ src/Controller/LogBookSecurityController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\LogBookUser;
+use PHPUnit\Runner\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Ldap\Exception\ConnectionException;
+use Symfony\Component\Ldap\Ldap;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -21,6 +24,57 @@ class LogBookSecurityController extends Controller
      */
     public function logout(Request $request){
 
+    }
+
+    protected function ldapLogin($user, $password){
+        $ret_arr = array();
+        try{
+            $ldap_server = getenv("LDAP_SERVER");
+            $dn = getenv("LDAP_DN");
+
+            $LDAP_DEFAULT_DOMAIN = getenv("LDAP_DEFAULT_DOMAIN");
+            $LDAP_USERNAME_STR = getenv("LDAP_USERNAME_STR");
+            $LDAP_EMAIL_STR = getenv("LDAP_EMAIL_STR");
+            $LDAP_FULLNAME_STR = getenv("LDAP_FULLNAME_STR");
+            $LDAP_LASTNAME_STR = getenv("LDAP_LASTNAME_STR");
+            $LDAP_FIRSTNAME = getenv("LDAP_FIRSTNAME");
+            $LDAP_ID_STR = getenv("LDAP_ID_STR");
+            $LDAP_MOBILE_STR = getenv("LDAP_MOBILE_STR");
+            $LDAP_SITECODE_STR = getenv("LDAP_SITECODE_STR");
+
+            $ldap = Ldap::create('ext_ldap', array('connection_string' => 'ldap://' . $ldap_server));
+            $domain_sufix = explode("\\", $user);
+            if(count($domain_sufix) == 2)
+            {
+                $user = $domain_sufix[1];
+                $domain = $domain_sufix[0];
+            }
+            else{
+                $domain = $LDAP_DEFAULT_DOMAIN;
+            }
+            $userWithDomain = $domain . '\\' . $user;
+
+            $ldap->bind($userWithDomain, $password);
+            $query = $ldap->query($dn, "(samaccountname=$user)", array(
+                "maxItems" => 1
+            ));
+            $results = $query->execute();
+
+            foreach ($results as $entry) {
+                $ret_arr["username"] = $entry->getAttribute("$LDAP_USERNAME_STR")[0];
+                $ret_arr["email"] = $entry->getAttribute("$LDAP_EMAIL_STR")[0];
+                $ret_arr["lastName"] = $entry->getAttribute("$LDAP_LASTNAME_STR")[0];
+                $ret_arr["firstName"] = $entry->getAttribute("$LDAP_FIRSTNAME")[0];
+                $ret_arr["fullName"] = $entry->getAttribute("$LDAP_FULLNAME_STR")[0];
+                $ret_arr["employeeId"] = $entry->getAttribute("$LDAP_ID_STR")[0];
+                $ret_arr["sitecode"] = $entry->getAttribute($LDAP_SITECODE_STR)[0];
+                $ret_arr["mobile"] = $entry->getAttribute("$LDAP_MOBILE_STR")[0];
+            }
+        }
+        catch (ConnectionException $ex){
+            throw new AuthenticationException("LDAP: " . $ex->getMessage());
+        }
+        return $ret_arr;
     }
 
     /**
@@ -38,8 +92,12 @@ class LogBookSecurityController extends Controller
             if($request->isMethod('POST')){
                 $user_name = $request->request->get('_username');
                 $password = $request->request->get('_password');
-                // Retrieve the security encoder of symfony
+//                $use_ldap = getenv("USE_LDAP");
+//                if($use_ldap === "true"){
+//                    $ldapUser = $this->ldapLogin($user_name, $password);
+//                }
 
+                // Retrieve the security encoder of symfony
                 $user_manager = $this->getDoctrine()->getManager()->getRepository("App:LogBookUser");
                 /** @var LogBookUser $user */
                 $user = $user_manager->loadUserByUsername($user_name);

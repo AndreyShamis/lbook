4ec618d8a4d209d204762b8edf6dbfd0688aff82
Andrey Shamis <lolnik@gmail.com>
2018-03-11 13:56:18 +0200

    Preparation to move OsType to Embeddedable
    Change-Id: I4652b2b50be17b6be3f15de1d5b7b1f4b053fb69
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Form/LogBookSetupType.php
+++ src/Form/LogBookSetupType.php
@@ -2,8 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\LogBookSetup;
-use App\Model\OsType;
+use App\Entity\OsType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;

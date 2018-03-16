7a57b1c0d8ce38a24065f0d7ba5e0a3895bddd2c
Andrey Shamis <lolnik@gmail.com>
2018-03-16 10:39:27 +0200

    Add paginator and dates to setup
    Change-Id: I8634c6fdde60295e69a8292aeb84369d724d3df3
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Controller/LogBookSetupController.php
+++ src/Controller/LogBookSetupController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use App\Entity\LogBookCycle;
 use App\Entity\LogBookSetup;
 use App\Entity\LogBookTest;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\ORM\PersistentCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * Setup controller.
@@ -19,22 +23,72 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class LogBookSetupController extends Controller
 {
+    protected $index_size = 50;
+    /**
+     * Paginator Helper
+     *
+     * Pass through a query object, current page & limit
+     * the offset is calculated from the page and limit
+     * returns an `Paginator` instance, which you can call the following on:
+     *
+     *     $paginator->getIterator()->count() # Total fetched (ie: `5` posts)
+     *     $paginator->count() # Count of ALL posts (ie: `20` posts)
+     *     $paginator->getIterator() # ArrayIterator
+     *
+     * @param Query|QueryBuilder $dql  A Doctrine ORM query or query builder.
+     * @param integer            $page  Current page (defaults to 1)
+     * @param integer            $limit The total number per page (defaults to 5)
+     *
+     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     */
+    public function paginate($dql, $page = 1, $limit = 20)
+    {
+        $paginator = new Paginator($dql);
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1)) // Offset
+            ->setMaxResults($limit); // Limit
+        return $paginator;
+    }
 
     /**
      * Lists all setup entities.
      *
-     * @Route("/", name="setup_index")
+     * @Route("/page/{page}", name="setup_index")
      * @Method("GET")
+     * @Template(template="lbook/setup/index.html.twig")
+     * @param int $page
+     * @return array
      */
-    public function index()
+    public function index(int $page=1)
     {
         $em = $this->getDoctrine()->getManager();
+        $cycleRepo = $em->getRepository('App:LogBookSetup');
+        $query = $cycleRepo->createQueryBuilder('t')
+            ->orderBy('t.id', "DESC");
+        $paginator = $this->paginate($query, $page, $this->index_size);
+        //$posts = $this->getAllPosts($page); // Returns 5 posts out of 20
+        // You can also call the count methods (check PHPDoc for `paginate()`)
+        //$totalPostsReturned = $paginator->getIterator()->count(); # Total fetched (ie: `5` posts)
+        $totalPosts = $paginator->count(); # Count of ALL posts (ie: `20` posts)
+        $iterator = $paginator->getIterator(); # ArrayIterator
 
-        $setups = $em->getRepository('App:LogBookSetup')->findAll();
-
-        return $this->render('lbook/setup/index.html.twig', array(
-            'setups' => $setups,
-        ));
+        $maxPages = ceil($totalPosts / $this->index_size);
+        $thisPage = $page;
+        return array(
+            //'setups' => $setups,
+            'size'      => $totalPosts,
+            'maxPages'  => $maxPages,
+            'thisPage'  => $thisPage,
+            'iterator'  => $iterator,
+            'paginator' => $paginator,
+        );
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $setups = $em->getRepository('App:LogBookSetup')->findAll();
+//
+//        return $this->render('lbook/setup/index.html.twig', array(
+//            'setups' => $setups,
+//        ));
     }
 
     /**

76beeaa301def948fc33e5be31ab2296c3b21dcc
Andrey Shamis <lolnik@gmail.com>
2018-03-19 15:10:08 +0200

    Test Iterator for delete
    Change-Id: I66b2045022efcdd2af6d25284b7fef1415738de7
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Repository/LogBookTestRepository.php
+++ src/Repository/LogBookTestRepository.php
@@ -6,6 +6,7 @@ use App\Entity\LogBookCycle;
 use App\Entity\LogBookTest;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class LogBookTestRepository extends ServiceEntityRepository
 {
@@ -45,12 +46,54 @@ class LogBookTestRepository extends ServiceEntityRepository
      */
     public function deleteByCycle(LogBookCycle $cycle): void
     {
+//        $stopwatchGetRepo = new Stopwatch();
+//        $stopwatchLogDelete = new Stopwatch();
+//        $stopwatchGetRepo->start('getRepo');
+
         $msgRepo = $this->getEntityManager()->getRepository('App:LogBookMessage');
+//        $eventGetRepo = $stopwatchGetRepo->stop('getRepo');
+//        echo "eventGetRepo time (milliseconds):";
+//        echo $eventGetRepo->getDuration() . "<br/> Log:";
+
+//        $stopwatchLogDelete->start('logDelete');
+
+
+//        $cycleRepo = $this->getEntityManager()->getRepository('App:LogBookTest');
+//        $qd = $cycleRepo->createQueryBuilder('t')
+//            ->where('t.cycle = :cycle')
+//            ->setParameter('cycle', $cycle->getId());
+//        $query = $qd->getQuery();
+//        $iterableResult = $query->iterate();
+//
+//        while (($row = $iterableResult->next()) !== false) {;
+//            /** @var LogBookTest $test */
+//            $msgRepo->deleteByTestId($row[0]->getId());
+////            $stopwatchLogDelete->lap("logDelete");
+//            $this->delete($row[0]);
+////            $stopwatchLogDelete->lap("logDelete");
+//            $this->_em->detach($row[0]);
+////            $event = $stopwatchLogDelete->stop('logDelete');
+////            $ar = $event->getPeriods();
+////            echo "Periods:<pre>";
+////            print_r($ar);
+////            echo "</pre>";
+////            exit();
+//        }
+
         foreach ($cycle->getTests() as $test){
             /** @var LogBookTest $test */
             $msgRepo->deleteByTestId($test->getId());
+            //$stopwatchLogDelete->lap("logDelete");
             $this->delete($test);
+            //$stopwatchLogDelete->lap("logDelete");
             $this->_em->detach($test);
+            //$event = $stopwatchLogDelete->stop('logDelete');
+//            $ar = $event->getPeriods();
+//            echo "Periods:<pre>";
+//            print_r($ar);
+//            echo "</pre>";
+//
+//            exit();
         }
         /**
          * Additional clean | optional and may be removed

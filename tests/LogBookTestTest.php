f8a8ab249d79122928ed71092b9f92c7bfc12257
Andrey Shamis <lolnik@gmail.com>
2018-03-23 16:55:48 +0300

    Improve tests/LogBookTest
    Change-Id: If86ac3b3f117749385da0314cba764129d0f5b9a
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- tests/LogBookTestTest.php
+++ tests/LogBookTestTest.php
@@ -4,7 +4,6 @@ namespace App\Tests;
 
 use App\Entity\LogBookTest;
 use DateTime;
-use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\TestCase;
 
 class LogBookTestTest extends TestCase
@@ -13,9 +12,8 @@ class LogBookTestTest extends TestCase
      * @param DateTime $timeStart
      * @param DateTime $timeEnd
      * @param bool $positive
-     * @throws AssertionFailedError
      */
-    protected function verify(DateTime &$timeStart, DateTime &$timeEnd, bool $positive = true)
+    protected function verify(DateTime $timeStart, DateTime $timeEnd, bool $positive = true): void
     {
         $test = new LogBookTest();
         $diff = $timeEnd->getTimestamp() - $timeStart->getTimestamp();
@@ -23,66 +21,74 @@ class LogBookTestTest extends TestCase
         $test->setTimeEnd($timeEnd);
         $test->calculateRunTime();
         $runTime = $test->getTimeRun();
-        if($positive){
-            $this->assertTrue($diff === $runTime);
-        }
-        else{
-            $this->assertTrue($diff === ($runTime*-1));
+        if ($positive) {
+            $this->assertSame($diff, $runTime);
+        } else {
+            $this->assertSame($diff, $runTime*-1);
         }
     }
 
-    public function testRunTimeCalculationPositiveDays()
+    protected function rand($min = 0, $max = 0): int
     {
-        for($x=0; $x < 100; $x++){
-            $timeStart = new DateTime('+' . rand(1, 10) . ' days');
-            $timeEnd = new DateTime('+' . rand(10, 100) . ' days');
+        try {
+            return random_int($min, $max);
+        } catch (\Exception $e) {
+            return 0;
+        }
+    }
+
+    public function testRunTimeCalculationPositiveDays(): void
+    {
+        for ($x=0; $x < 100; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(1, 10) . ' days');
+            $timeEnd = new DateTime('+' . $this->rand(10, 100) . ' days');
             $this->verify($timeStart, $timeEnd);
         }
     }
 
-    public function testRunTimeCalculationPositiveHours()
+    public function testRunTimeCalculationPositiveHours(): void
     {
-        for($x=0; $x < 1000; $x++){
-            $timeStart = new DateTime('+' . rand(1, 1000) . ' hours');
-            $timeEnd = new DateTime('+' . rand(1000, 10000) . ' hours');
+        for ($x=0; $x < 1000; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(1, 1000) . ' hours');
+            $timeEnd = new DateTime('+' . $this->rand(1000, 10000) . ' hours');
             $this->verify($timeStart, $timeEnd);
         }
     }
 
-    public function testRunTimeCalculationPositiveMinutes()
+    public function testRunTimeCalculationPositiveMinutes(): void
     {
-        for($x=0; $x < 1000; $x++){
-            $timeStart = new DateTime('+' . rand(1, 10000) . ' minutes');
-            $timeEnd = new DateTime('+' . rand(10000, 100000) . ' minutes');
+        for ($x=0; $x < 1000; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(1, 10000) . ' minutes');
+            $timeEnd = new DateTime('+' . $this->rand(10000, 100000) . ' minutes');
             $this->verify($timeStart, $timeEnd);
         }
     }
 
-    public function testRunTimeCalculationPositiveSeconds()
+    public function testRunTimeCalculationPositiveSeconds(): void
     {
-        for($x=0; $x < 1000; $x++){
-            $timeStart = new DateTime('+' . rand(1, 100000) . ' seconds');
-            $timeEnd = new DateTime('+' . rand(100000, 1000000) . ' seconds');
+        for ($x=0; $x < 1000; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(1, 100000) . ' seconds');
+            $timeEnd = new DateTime('+' . $this->rand(100000, 1000000) . ' seconds');
             $this->verify($timeStart, $timeEnd);
         }
     }
 
-    public function testRunTimeCalculationNegative()
+    public function testRunTimeCalculationNegative(): void
     {
-        for($x=0; $x < 100; $x++){
-            $timeStart = new DateTime('+' . rand(10, 100) . ' days');
-            $timeEnd = new DateTime('+' . rand(0, 10) . ' days');
+        for ($x=0; $x < 100; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(10, 100) . ' days');
+            $timeEnd = new DateTime('+' . $this->rand(0, 10) . ' days');
             $this->verify($timeStart, $timeEnd, false);
         }
     }
 
-    public function testRunTimeCalculationBoth()
+    public function testRunTimeCalculationBoth(): void
     {
-        for($x=0; $x < 300; $x++){
-            $timeStart = new DateTime('+' . rand(1, 100) . ' days');
-            $timeEnd = new DateTime('+' . rand(50, 100) . ' days');
+        for ($x=0; $x < 300; $x++) {
+            $timeStart = new DateTime('+' . $this->rand(1, 100) . ' days');
+            $timeEnd = new DateTime('+' . $this->rand(50, 100) . ' days');
             $diff = $timeEnd->getTimestamp() - $timeStart->getTimestamp();
-            $this->verify($timeStart, $timeEnd, $diff < 0 ? false : true);
+            $this->verify($timeStart, $timeEnd, $diff >= 0);
         }
     }
 }

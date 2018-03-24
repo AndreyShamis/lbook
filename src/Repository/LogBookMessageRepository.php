6b6f129d710693a984440fd9b953070e74581d9c
Andrey Shamis <lolnik@gmail.com>
2018-03-24 15:26:21 +0300

    Clean Test Repo code
    Change-Id: I03e7c8d329b3177d2ecb232f1a41c9b70ecdff5f
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Repository/LogBookMessageRepository.php
+++ src/Repository/LogBookMessageRepository.php
@@ -9,11 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class LogBookMessageRepository extends ServiceEntityRepository
 {
-    /**
-     * @var array Keep hashed entity
-     */
-//    protected static $_hashedData = array();
-
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, LogBookMessage::class);
@@ -26,24 +21,22 @@ class LogBookMessageRepository extends ServiceEntityRepository
      */
     public function Create(array $criteria, $flush = true): LogBookMessage
     {
-
-
-        $entity = null; //$this->findOneBy($criteria);
-        if (null === $entity) {
-            $entity = new LogBookMessage();
-            $entity->setMessage($criteria['message']);
-            $entity->setChain($criteria['chain']);
-            $entity->setMsgType($criteria['msgType']);
-            $entity->setLogTime($criteria['logTime']);
-            $entity->setTest($criteria['test']);
-            $this->_em->persist($entity);
-            if($flush){
-                $this->_em->flush($entity);
-            }
+        $entity = new LogBookMessage();
+        $entity->setMessage($criteria['message']);
+        $entity->setChain($criteria['chain']);
+        $entity->setMsgType($criteria['msgType']);
+        $entity->setLogTime($criteria['logTime']);
+        $entity->setTest($criteria['test']);
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush($entity);
         }
         return $entity;
     }
 
+    /**
+     * @param LogBookTest $test
+     */
     public function deleteByTest(LogBookTest $test): void
     {
         $qd = $this->createQueryBuilder('t')
@@ -54,7 +47,10 @@ class LogBookMessageRepository extends ServiceEntityRepository
         $query->execute();
     }
 
-    public function deleteByTestId($testId): void
+    /**
+     * @param int $testId
+     */
+    public function deleteByTestId(int $testId): void
     {
         $qd = $this->createQueryBuilder('t')
             ->delete()
@@ -63,52 +59,4 @@ class LogBookMessageRepository extends ServiceEntityRepository
         $query = $qd->getQuery();
         $query->execute();
     }
-//    /**
-//     * @param array $criteria
-//     * @param bool $flush
-//     * @return LogBookMessage
-//     */
-//    public function findOneOrCreate(array $criteria, $flush = true)
-//    {
-////        $add_hash = true;
-////        if(isset(self::$_hashedData[$criteria['name']])){
-////            $entity = self::$_hashedData[$criteria['name']];
-////            $add_hash = false;
-////        }
-////        else{
-////            $entity = $this->findOneBy($criteria);
-////        }
-//
-//        $entity = null; //$this->findOneBy($criteria);
-//        if (null === $entity) {
-//            $entity = new LogBookMessage();
-//            $entity->setMessage($criteria['message']);
-//            $entity->setChain($criteria['chain']);
-//            $entity->setMsgType($criteria['msgType']);
-//            $entity->setLogTime($criteria['logTime']);
-//            $entity->setTest($criteria['test']);
-//            $this->_em->persist($entity);
-//            if($flush){
-//                $this->_em->flush($entity);
-//            }
-//
-//
-//        }
-////        if($add_hash) {
-////            self::$_hashedData[$criteria['name']] = $entity;
-////        }
-//        return $entity;
-//    }
-    /*
-    public function findBySomething($value)
-    {
-        return $this->createQueryBuilder('l')
-            ->where('l.something = :value')->setParameter('value', $value)
-            ->orderBy('l.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }

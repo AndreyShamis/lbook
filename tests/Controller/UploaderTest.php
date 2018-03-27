43951894e9d1844ac062857672b7eb253bcdd3c9
Andrey Shamis <lolnik@gmail.com>
2018-03-27 20:41:04 +0300

    Add uploadCli test
    Change-Id: Ifc7f90cf05acf6f7276d6728061a62986455ac95
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ tests/Controller/UploaderTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * User: Andrey Shamis
+ * email: lolnik@gmail.com
+ * Date: 27/03/18
+ * Time: 20:18
+ */
+
+namespace App\Tests\Controller;
+
+use App\Utils\RandomString;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+
+class UploaderTest extends LogBookApplicationTestCase
+{
+    /**
+     *
+     * @param null|string $name
+     * @param array $data
+     * @param string $dataName
+     * @throws \Exception
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->setUp();
+    }
+
+    public function testUploadCli():void
+    {
+        $token = RandomString::generateRandomString(20);
+        $filePathSrc = realpath(__DIR__) . '/ForUpload/PASS__network_WiFi_Perf.11g';
+        $filePath = realpath(__DIR__) . '/PASS__network_WiFi_Perf.11g';
+        copy($filePathSrc, $filePath);
+        $file = new UploadedFile(
+            $filePath,
+            'PASS__network_WiFi_Perf.11g',
+            'text/plain',
+            filesize($filePath)
+        );
+        $client = $this->getClient();
+//        request(
+//            $method,
+//            $uri,
+//            array $parameters = array(),
+//            array $files = array(),
+//            array $server = array(),
+//            $content = null,
+//            $changeHistory = true
+//        )
+        $client->request(
+            'POST',
+            '/upload/new_cli',
+            array(
+                'token' => $token,
+                'setup' => 'Test Setup',
+                //'cycle' => 'asdasdasdasd',
+
+            ),
+            array('file' => $file),
+            array(
+                //'CONTENT_TYPE'          => 'multipart/form-data;',
+                'HTTP_REFERER'          => '/upload/new_cli',
+                //'HTTP_X-Requested-With' => 'XMLHttpRequest',
+            )
+        );
+        $this->assertSame(Response::HTTP_OK, $this->getClient()->getResponse()->getStatusCode());
+        echo  $this->getClient()->getResponse()->getContent();
+    }
+}

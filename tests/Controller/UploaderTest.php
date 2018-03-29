78c0b55b79c7be74408f76a8c42c87151a280da1
Andrey Shamis <lolnik@gmail.com>
2018-03-29 08:53:59 +0300

    Add more check to uploadCLi
    Change-Id: I2b0a649903994a02ce6028ace0627f50b6b94824
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- tests/Controller/UploaderTest.php
+++ tests/Controller/UploaderTest.php
@@ -30,11 +30,37 @@ class UploaderTest extends LogBookApplicationTestCase
         $this->setUp();
     }
 
+
+    protected function resource_copy($src, $dst): void
+    {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file !== '.' ) && ( $file !== '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    $this->resource_copy($src . '/' . $file, $dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
     /**
      * @throws \Doctrine\ORM\ORMException
      */
     public function testUploadCli():void
     {
+        $token = RandomString::generateRandomString(20);
+        $setupName = 'Test Setup';
+        $postParams = array(
+            'token' => $token,
+            'setup' => $setupName,
+            //'cycle' => 'asdasdasdasd',
+        );
+        $postHeader = array('HTTP_REFERER' => '/upload/new_cli',);
         $testRepo = self::$entityManager->getRepository(LogBookTest::class);
         $passTestName = 'network_WiFi_Perf.11g';
         $passFileName = 'PASS__' . $passTestName;
@@ -42,79 +68,40 @@ class UploaderTest extends LogBookApplicationTestCase
         $errorFileName = 'ERROR__' . $errorTestName;
         $failTestName = 'network_WiFi_Perf.ht40';
         $failFileName = 'FAIL__' . $failTestName;
-        $token = RandomString::generateRandomString(20);
-        $file1PathSrc = realpath(__DIR__) . '/ForUpload/' . $passFileName;
-        $file2PathSrc = realpath(__DIR__) . '/ForUpload/' . $errorFileName;
-        $file3PathSrc = realpath(__DIR__) . '/ForUpload/' . $failFileName;
-        $filePath1 = realpath(__DIR__) . '/' . $passFileName;
-        $filePath2 = realpath(__DIR__) . '/' . $errorFileName;
-        $filePath3 = realpath(__DIR__) . '/' . $failFileName;
-        copy($file1PathSrc, $filePath1);
-        copy($file2PathSrc, $filePath2);
-        copy($file3PathSrc, $filePath3);
 
-        $setupName = 'Test Setup';
+//        $file1PathSrc = realpath(__DIR__) . '/ForUpload/' . $passFileName;
+//        $file2PathSrc = realpath(__DIR__) . '/ForUpload/' . $errorFileName;
+//        $file3PathSrc = realpath(__DIR__) . '/ForUpload/' . $failFileName;
+//
+//        $filePath1 = realpath(__DIR__) . '/' . $passFileName;
+//        $filePath2 = realpath(__DIR__) . '/' . $errorFileName;
+//        $filePath3 = realpath(__DIR__) . '/' . $failFileName;
+//
+//        copy($file1PathSrc, $filePath1);
+//        copy($file2PathSrc, $filePath2);
+//        copy($file3PathSrc, $filePath3);
+
+        $this->resource_copy('/ForUpload/', '/');
 
         $file1 = new UploadedFile($filePath1, $passFileName, 'text/plain', filesize($filePath1));
         $file2 = new UploadedFile($filePath2, $errorFileName, 'text/plain', filesize($filePath2));
         $file3 = new UploadedFile($filePath3, $failFileName, 'text/plain', filesize($filePath3));
         $client = $this->getClient();
 
-        $client->request('POST', '/upload/new_cli',
-            array(
-                'token' => $token,
-                'setup' => $setupName,
-                //'cycle' => 'asdasdasdasd',
-            ),
-            array('file' => $file1),
-            array('HTTP_REFERER' => '/upload/new_cli',)
-        );
+        $client->request('POST', '/upload/new_cli', $postParams, array('file' => $file1), $postHeader);
         $this->assertSame(Response::HTTP_OK, $this->getClient()->getResponse()->getStatusCode());
         $firstContent = $this->getClient()->getResponse()->getContent();
         //echo  $firstContent;
-        $this->assertRegExp('/Token provided \['.$token.'\]/', $firstContent);
-        $this->assertRegExp('/Cycle not found by token\. Parsing Setup/', $firstContent);
-        $this->assertRegExp('/Searching setup by NAME :'.$setupName.'/', $firstContent);
-        $this->assertRegExp('/Creating setup  :'.$setupName.'/', $firstContent);
-        $this->assertRegExp('/Cycle name not provided/', $firstContent);
-        $this->assertRegExp('/Generated cycle name/', $firstContent);
-        $this->assertRegExp('/Creating cycle/', $firstContent);
-        $this->assertRegExp('/Cycle created ID:\d+\./', $firstContent);
-        $this->assertRegExp('/Cycle found, take SETUP from cycle/', $firstContent);
-        $this->assertRegExp('/File name is :[\w\d]+'.$passFileName.'/', $firstContent);
-        $this->assertRegExp('/TestID is \d+\.$/', $firstContent);
+        $this->validateNotExistingTestResponse($firstContent, $setupName, $token, $passFileName);
 
-        /**
-         *
-         */
-        $client->request('POST', '/upload/new_cli',
-            array(
-                'token' => $token,
-                'setup' => $setupName,
-            ),
-            array('file' => $file2),
-            array('HTTP_REFERER' => '/upload/new_cli',
-            )
-        );
+        /** Second file upload */
+        $client->request('POST', '/upload/new_cli', $postParams, array('file' => $file2), $postHeader);
 
         $this->assertSame(Response::HTTP_OK, $this->getClient()->getResponse()->getStatusCode());
         $secondContent = $this->getClient()->getResponse()->getContent();
         //echo  $secondContent;
-        $this->assertRegExp('/Token provided \['.$token.'\]/', $secondContent);
-        $this->assertNotRegExp('/Cycle not found by token\. Parsing Setup/', $secondContent);
-        $this->assertNotRegExp('/Searching setup by NAME :'.$setupName.'/', $secondContent);
-        $this->assertNotRegExp('/Creating setup  :'.$setupName.'/', $secondContent);
-        $this->assertNotRegExp('/Cycle name not provided/', $secondContent);
-        $this->assertNotRegExp('/Generated cycle name/', $secondContent);
-        $this->assertNotRegExp('/Creating cycle/', $secondContent);
-        $this->assertNotRegExp('/Cycle created ID:\d+./', $secondContent);
-        $this->assertRegExp('/Cycle found, take SETUP from cycle/', $secondContent);
-        $this->assertRegExp('/File name is :[\w\d]+'.$errorFileName.'/', $secondContent);
-        $this->assertRegExp('/TestID is \d+\.$/', $secondContent);
-
+        $this->validateExistingTestResponse($secondContent, $setupName, $token, $errorFileName);
         $testId = $this->findTestIdInTest($secondContent);
-        //self::setUp();
-
 
         $test = $testRepo->find($testId);
         /** @var LogBookCycle $cycle */
@@ -128,52 +115,90 @@ class UploaderTest extends LogBookApplicationTestCase
         $this->assertEquals(2, $cycle->getTests()->count(), 'Check that cycle include 2 created test. count: ' . $cycle->getTests()->count());
         $this->assertEquals(50, $cycle->getPassRate(), 'Check that cycle pass rate is 50%: Actual = ' . $cycle->getPassRate());
 
-        /*
-         *
-         */
-        $client->request('POST', '/upload/new_cli',
-            array(
-                'token' => $token,
-                'setup' => $setupName,
-            ),
-            array('file' => $file3),
-            array('HTTP_REFERER' => '/upload/new_cli',
-            )
-        );
+        /** Third file upload */
+        $client->request('POST', '/upload/new_cli', $postParams, array('file' => $file3), $postHeader);
+
         $this->assertSame(Response::HTTP_OK, $this->getClient()->getResponse()->getStatusCode());
         $thirdResponse = $this->getClient()->getResponse()->getContent();
 
-        $this->assertRegExp('/Token provided \['.$token.'\]/', $thirdResponse);
-        $this->assertNotRegExp('/Cycle not found by token\. Parsing Setup/', $thirdResponse);
-        $this->assertNotRegExp('/Searching setup by NAME :'.$setupName.'/', $thirdResponse);
-        $this->assertNotRegExp('/Creating setup  :'.$setupName.'/', $thirdResponse);
-        $this->assertNotRegExp('/Cycle name not provided/', $thirdResponse);
-        $this->assertNotRegExp('/Generated cycle name/', $thirdResponse);
-        $this->assertNotRegExp('/Creating cycle/', $thirdResponse);
-        $this->assertNotRegExp('/Cycle created ID:\d+./', $thirdResponse);
-        $this->assertRegExp('/Cycle found, take SETUP from cycle/', $thirdResponse);
-        $this->assertRegExp('/File name is :[\w\d]+'.$failFileName.'/', $thirdResponse);
-        $this->assertRegExp('/TestID is \d+\.$/', $thirdResponse);
+        $this->validateExistingTestResponse($thirdResponse, $setupName, $token, $failFileName);
 
         $testId = $this->findTestIdInTest($thirdResponse);
-        //self::setUp();
 
         $testRepo = self::$entityManager->getRepository(LogBookTest::class);
         self::$entityManager->refresh($cycle);
         self::$entityManager->refresh($setup);
         $test = $testRepo->find($testId);
-        /** @var LogBookCycle $cycle3 */
-        $cycle3 = $test->getCycle();
-        /** @var LogBookSetup $setup */
-        $setup = $cycle3->getSetup();
+
         //echo "Setup cycles count " . count($setup->getCycles()) . "\n";
         $this->assertSame($failTestName, $test->getName(), 'Check that test name is same: Actual: ' . $test->getName() . ', expected ' . $failTestName);
         $this->assertSame(111, $test->getTimeRun(), 'Check that test RunTime is same: Actual: ' . $test->getTimeRun() . ', expected ' . 111);
         $this->assertEquals(1, $setup->getCycles()->count(), 'Check that Setup include 1 created cycle. count: ' . $setup->getCycles()->count());
-        $this->assertEquals(3, $cycle3->getTests()->count(), 'Check that cycle include 2 created test. count: ' . $cycle3->getTests()->count());
-        $this->assertEquals(33.33, $cycle3->getPassRate(), 'Check that cycle pass rate is 33.33%: Actual = ' . $cycle3->getPassRate());
+        $this->assertEquals(3, $cycle->getTests()->count(), 'Check that cycle include 2 created test. count: ' . $cycle->getTests()->count());
+        $this->assertEquals(33.33, $cycle->getPassRate(), 'Check that cycle pass rate is 33.33%: Actual = ' . $cycle->getPassRate());
+
+        /** Check Test Pass/Fail/Error counters */
+        $this->assertEquals(1, $cycle->getTestsPass(), 'Check that cycle Pass Count is 1: Actual = ' . $cycle->getTestsPass());
+        $this->assertEquals(1, $cycle->getTestsFail(), 'Check that cycle Fail Count is 1: Actual = ' . $cycle->getTestsFail());
+        $this->assertEquals(1, $cycle->getTestsError(), 'Check that cycle Error Count is 1: Actual = ' . $cycle->getTestsError());
+
+        /** Check Test Run times */
+        $period = 1437;
+        $testTimeSum = 572;
+        $this->assertEquals($period, $cycle->getPeriod(), 'Check that cycle Tests Period is '.$period.': Actual = ' . $cycle->getPeriod());
+        $this->assertEquals($testTimeSum, $cycle->getTestsTimeSum(), 'Check that cycle Tests [Run Time(tests_time_sum)] is '.$testTimeSum.': Actual = ' . $cycle->getTestsTimeSum());
+
+        /**   */
     }
 
+    /**
+     * @param string $stringResponse
+     * @param string $setupName
+     * @param string $token
+     * @param string $fileName
+     */
+    protected function validateExistingTestResponse(string &$stringResponse, string &$setupName, string &$token, string &$fileName): void
+    {
+        $this->assertNotRegExp('/Failed to generate cycle/', $stringResponse);
+        $this->assertRegExp('/Token provided \['.$token.'\]/', $stringResponse);
+        $this->assertNotRegExp('/Cycle not found by token\. Parsing Setup/', $stringResponse);
+        $this->assertNotRegExp('/Searching setup by NAME :'.$setupName.'/', $stringResponse);
+        $this->assertNotRegExp('/Creating setup  :'.$setupName.'/', $stringResponse);
+        $this->assertNotRegExp('/Cycle name not provided/', $stringResponse);
+        $this->assertNotRegExp('/Generated cycle name/', $stringResponse);
+        $this->assertNotRegExp('/Creating cycle/', $stringResponse);
+        $this->assertNotRegExp('/Cycle created ID:\d+./', $stringResponse);
+        $this->assertRegExp('/Cycle found, take SETUP from cycle/', $stringResponse);
+        $this->assertRegExp('/File name is :[\w\d]+'.$fileName.'/', $stringResponse);
+        $this->assertRegExp('/TestID is \d+\.$/', $stringResponse);
+    }
+
+    /**
+     * @param string $stringResponse
+     * @param string $setupName
+     * @param string $token
+     * @param string $fileName
+     */
+    protected function validateNotExistingTestResponse(string &$stringResponse, string &$setupName, string &$token, string &$fileName): void
+    {
+        $this->assertNotRegExp('/Failed to generate cycle/', $stringResponse);
+        $this->assertRegExp('/Token provided \['.$token.'\]/', $stringResponse);
+        $this->assertRegExp('/Cycle not found by token\. Parsing Setup/', $stringResponse);
+        $this->assertRegExp('/Searching setup by NAME :'.$setupName.'/', $stringResponse);
+        $this->assertRegExp('/Creating setup  :'.$setupName.'/', $stringResponse);
+        $this->assertRegExp('/Cycle name not provided/', $stringResponse);
+        $this->assertRegExp('/Generated cycle name/', $stringResponse);
+        $this->assertRegExp('/Creating cycle/', $stringResponse);
+        $this->assertRegExp('/Cycle created ID:\d+\./', $stringResponse);
+        $this->assertRegExp('/Cycle found, take SETUP from cycle/', $stringResponse);
+        $this->assertRegExp('/File name is :[\w\d]+'.$fileName.'/', $stringResponse);
+        $this->assertRegExp('/TestID is \d+\.$/', $stringResponse);
+    }
+
+    /**
+     * @param $input
+     * @return int
+     */
     protected function findTestIdInTest(&$input): int
     {
         preg_match('/TestID is (\d+)\./', $input, $matches);

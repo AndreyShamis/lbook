a1867e4ac86bc0639db025cbe234eff45a5e9313
Andrey Shamis <lolnik@gmail.com>
2018-07-01 18:39:43 +0300

    Custom search
    Change-Id: I1729f124352b7441267013342a2f88ab0b487240
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Form/LogBookVerdictType.php
+++ src/Form/LogBookVerdictType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class LogBookVerdictType extends AbstractType
 {
@@ -20,14 +23,30 @@ class LogBookVerdictType extends AbstractType
     }
 
     /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $form
+            ->add('name')
+        ;
+    }
+    /**
      * @param OptionsResolver $resolver
      * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
      */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            //'compound' => true,
             // uncomment if you want to bind to a class
             //'data_class' => LogBookVerdict::class,
         ]);
     }
+
+//    public function getParent()
+//    {
+//        return ChoiceType::class;
+//    }
 }

8423e1d52dae752cac545ffd2d3910bb6ca26f7c
Andrey Shamis <lolnik@gmail.com>
2018-07-06 11:13:07 +0300

    Fix migrate diff in user roles
    Change-Id: I217b1a54c8e2d729a6243c163fbd248ec2c9090e
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Entity/LogBookUser.php
+++ src/Entity/LogBookUser.php
@@ -90,7 +90,8 @@ class LogBookUser implements UserInterface, \Serializable
     protected $isActive;
 
     /**
-     * @ORM\Column(type="json", columnDefinition="TEXT NOT NULL")
+     * @var array
+     * @ORM\Column(type="json")
      */
     protected $roles = [];
 

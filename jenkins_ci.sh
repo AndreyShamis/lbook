6ae257de8f98101e748cfdb45591a22cf42ccab3
Andrey Shamis <lolnik@gmail.com>
2018-07-06 09:16:21 +0300

    Some comment change in CI
    Change-Id: I9196006e9522d2cde40914372a0fdf37a8b97740
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- jenkins_ci.sh
+++ jenkins_ci.sh
@@ -35,7 +35,7 @@ restore_proxy(){
 export sym="php bin/console"
 
 # Requirements
-#sudo apt install php7.2-ldap php7.2-zip php7.2-xml php7.2-mbstring
+#sudo apt install php7.2-ldap php7.2-zip php7.2-xml php7.2-mbstring php7.2-mysql
 
 save_proxy
 

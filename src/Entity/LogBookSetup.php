47629316765ce6123d0df6d429cd98357f313b74
Andrey Shamis <lolnik@gmail.com>
2018-11-02 16:57:40 +0200

    NewBOT
    Change-Id: I17deede35292a81c913bfaddaad718b4809c8f04
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/Entity/LogBookSetup.php
+++ src/Entity/LogBookSetup.php
@@ -103,6 +103,13 @@ class LogBookSetup
      */
     protected $updatedAt;
 
+    /**
+     * @var integer
+     * Value in days used for delete Cycles
+     * @ORM\Column(name="retention_policy", type="smallint", options={"unsigned"=true, "default"="400"})
+     */
+    protected $retentionPolicy = 400;
+
     public static $MIN_NAME_LEN = 2;
     public static $MAX_NAME_LEN = 250;
 
@@ -115,6 +122,23 @@ class LogBookSetup
         $this->setCreatedAt();
         $this->moderators = new ArrayCollection();
         $this->cycles = new ArrayCollection();
+        $this->setRetentionPolicy(90);
+    }
+
+    /**
+     * @return int
+     */
+    public function getRetentionPolicy(): int
+    {
+        return $this->retentionPolicy;
+    }
+
+    /**
+     * @param int $retentionPolicy
+     */
+    public function setRetentionPolicy(int $retentionPolicy): void
+    {
+        $this->retentionPolicy = $retentionPolicy;
     }
 
     /**

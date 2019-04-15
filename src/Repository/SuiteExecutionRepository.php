695d1cd34bb47a0998cc8d56ee8acce64caaa1d3
Andrey Shamis <andrey.shamis@intel.com>
2019-04-15 14:16:34 +0300

    Fix and improve suiteExecution insertation
    Change-Id: I4c7c2e110e59456fefe8d24909a9d2df527ddf3c
    Signed-off-by: Andrey Shamis <andrey.shamis@intel.com>

--- src/Repository/SuiteExecutionRepository.php
+++ src/Repository/SuiteExecutionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\SuiteExecution;
+use App\Utils\RandomString;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -27,14 +28,46 @@ class SuiteExecutionRepository extends ServiceEntityRepository
      */
     public function findOneOrCreate(array $criteria): SuiteExecution
     {
-        $entity = $this->findOneBy(
-            array(
-                'summary' => $criteria['summary'],
-                'testingLevel' => $criteria['testing_level'],
-                'productVersion' => $criteria['product_version'],
-                'platform' => $criteria['platform'],
-                'chip' => $criteria['chip']
-            ));
+        $publish = false;
+        if (!array_key_exists('datetime', $criteria)) {
+            $criteria['datetime'] = RandomString::generateRandomStringMd5(40);
+        }
+        if (array_key_exists('publish', $criteria) && $criteria['publish'] === true) {
+            $publish = true;
+        }
+        $entity = null;
+
+        $job_name = $build_tag = $target_arch = $arch = '';
+        if (array_key_exists('job_name', $criteria)) {
+            $job_name = $criteria['job_name'];
+        }
+        if (array_key_exists('build_tag', $criteria)) {
+            $build_tag = $criteria['build_tag'];
+        }
+        if (array_key_exists('target_arch', $criteria)) {
+            $target_arch = $criteria['target_arch'];
+        }
+        if (array_key_exists('arch', $criteria)) {
+            $arch = $criteria['arch'];
+        }
+        try {
+            $entity = $this->findOneBy(
+                array(
+                    'summary' => $criteria['summary'],
+                    'testingLevel' => $criteria['testing_level'],
+                    'productVersion' => $criteria['product_version'],
+                    'platform' => $criteria['platform'],
+                    'chip' => $criteria['chip'],
+                    'publish' => $publish,
+                    'jobName' => $job_name,
+                    'buildTag' => $build_tag,
+                    'targetArch' => $target_arch,
+                    'arch' => $arch,
+                    'datetime' => $criteria['datetime'],
+                    'cycle' => null
+                ));
+        } catch (\Exception $ex) {}
+
         if (null === $entity) {
             $entity = new SuiteExecution();
             $entity->setSummary($criteria['summary']);
@@ -42,26 +75,16 @@ class SuiteExecutionRepository extends ServiceEntityRepository
             $entity->setProductVersion($criteria['product_version']);
             $entity->setPlatform($criteria['platform']);
             $entity->setChip($criteria['chip']);
-
-            if (array_key_exists('publish', $criteria) && $criteria['publish'] === true) {
-                $entity->setPublish($criteria['publish']);
-            }
-
+            $entity->setPublish($publish);
+            $entity->setJobName($criteria['job_name']);
+            $entity->setBuildTag($criteria['build_tag']);
+            $entity->setTargetArch($criteria['target_arch']);
+            $entity->setArch($criteria['arch']);
+            $entity->setDatetime($criteria['datetime']);
             if (array_key_exists('description', $criteria)) {
                 $entity->setDescription($criteria['description']);
             }
-            if (array_key_exists('job_name', $criteria)) {
-                $entity->setJobName($criteria['job_name']);
-            }
-            if (array_key_exists('build_tag', $criteria)) {
-                $entity->setBuildTag($criteria['build_tag']);
-            }
-            if (array_key_exists('target_arch', $criteria)) {
-                $entity->setTargetArch($criteria['target_arch']);
-            }
-            if (array_key_exists('arch', $criteria)) {
-                $entity->setArch($criteria['arch']);
-            }
+
             if (array_key_exists('test_plan_url', $criteria)) {
                 $entity->setTestPlanUrl($criteria['test_plan_url']);
             }

1b08ba8b37cd2564efaaead49e431eed383f6641
Andrey Shamis <andrey.shamis@intel.com>
2019-11-15 15:04:16 +0200

    Add hosts, fix last seen update for host
    Change-Id: I6f257ac318d1e7baf9be2159359f1352461a8b76
    Signed-off-by: Andrey Shamis <andrey.shamis@intel.com>

---
+++ src/Controller/LogBookHostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\HostRepository;
+use App\Service\PagePaginator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Host controller.
+ *
+ * @Route("hosts")
+ */
+class LogBookHostController extends AbstractController
+{
+    protected $index_size = 500;
+
+    /**
+     * @Route("/", name="hosts_index")
+     * @param HostRepository $hosts
+     * @param PagePaginator $pagePaginator
+     * @param int $page
+     * @return Response
+     */
+    public function index(HostRepository $hosts, PagePaginator $pagePaginator, $page = 1)
+    {
+        $query = $hosts->createQueryBuilder('hosts')
+            ->orderBy('hosts.updatedAt', 'DESC');
+        $paginator = $pagePaginator->paginate($query, $page, $this->index_size);
+        $totalPosts = $paginator->count();
+        $iterator = $paginator->getIterator();
+
+        $iterator->rewind();
+
+        $maxPages = ceil($totalPosts / $this->index_size);
+        $thisPage = $page;
+        return $this->render('lbook/host/index.html.twig', array(
+            'size'      => $totalPosts,
+            'maxPages'  => $maxPages,
+            'thisPage'  => $thisPage,
+            'hosts'     => $iterator,
+            'paginator' => $paginator,
+        ));
+    }
+}

2b2d90a2decb682ed96817fdb332dcab35ac0979
Andrey Shamis <andrey.shamis@intel.com>
2019-11-17 12:50:27 +0200

    Small improvments
    Change-Id: Icdb88a542bf7b9fe46ced0b171749241ff5c8a28
    Signed-off-by: Andrey Shamis <andrey.shamis@intel.com>

--- src/Controller/LogBookHostController.php
+++ src/Controller/LogBookHostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Host;
 use App\Repository\HostRepository;
+use App\Repository\SuiteExecutionRepository;
 use App\Service\PagePaginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,14 +18,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class LogBookHostController extends AbstractController
 {
     protected $index_size = 500;
+    protected $show_suites_size = 500;
 
     /**
-     * @Route("/", name="hosts_index")
-     * @param HostRepository $hosts
-     * @param PagePaginator $pagePaginator
-     * @param int $page
-     * @return Response
-     */
+ * @Route("/", name="hosts_index")
+ * @param HostRepository $hosts
+ * @param PagePaginator $pagePaginator
+ * @param int $page
+ * @return Response
+ */
     public function index(HostRepository $hosts, PagePaginator $pagePaginator, $page = 1)
     {
         $query = $hosts->createQueryBuilder('host')
@@ -37,6 +40,38 @@ class LogBookHostController extends AbstractController
         $maxPages = ceil($totalPosts / $this->index_size);
         $thisPage = $page;
         return $this->render('lbook/host/index.html.twig', array(
+            'size'      => $totalPosts,
+            'maxPages'  => $maxPages,
+            'thisPage'  => $thisPage,
+            'hosts'     => $iterator,
+            'paginator' => $paginator,
+        ));
+    }
+
+    /**
+     * @Route("/show/{id}", name="host_show")
+     * @param SuiteExecutionRepository $suites
+     * @param PagePaginator $pagePaginator
+     * @param int $page
+     * @return Response
+     */
+    public function show(Host $host, SuiteExecutionRepository $suites, PagePaginator $pagePaginator, $page = 1)
+    {
+        $query = $suites->createQueryBuilder('suite_execution')
+            ->orderBy('suite_execution.updatedAt', 'DESC')
+            ->andWhere('suite_execution.host = :host')
+            ->setParameter('host', $host->getId());
+
+        $paginator = $pagePaginator->paginate($query, $page, $this->show_suites_size);
+        $totalPosts = $paginator->count();
+        $iterator = $paginator->getIterator();
+
+        $iterator->rewind();
+
+        $maxPages = ceil($totalPosts / $this->index_size);
+        $thisPage = $page;
+        return $this->render('lbook/host/show.html.twig', array(
+            'host'      => $host,
             'size'      => $totalPosts,
             'maxPages'  => $maxPages,
             'thisPage'  => $thisPage,

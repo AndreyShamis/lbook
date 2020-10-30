e52077d5f8e6664df599c8bc86e75048993a5dfd
Andrey Shamis <lolnik@gmail.com>
2020-10-30 16:36:56 +0200

    Add cycleReport edit hostory
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I53d92956d41771894b0143c383f796051dbfbafa
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ src/Repository/CycleReportEditHistoryRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CycleReportEditHistory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method CycleReportEditHistory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CycleReportEditHistory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CycleReportEditHistory[]    findAll()
+ * @method CycleReportEditHistory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CycleReportEditHistoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CycleReportEditHistory::class);
+    }
+
+    /**
+     * @param array $criteria
+     * @return CycleReportEditHistory
+     * @throws ORMException
+     */
+    public function findOneOrCreate(array $criteria): CycleReportEditHistory
+    {
+        $entity = $this->findOneBy($criteria);
+        if (null === $entity) {
+            $entity = new CycleReportEditHistory();
+            $entity->setUser($criteria['user']);
+            $entity->setDiff($criteria['diff']);
+            $entity->setHappenedAt($criteria['happenedAt']);
+            $entity->setReport($criteria['report']);
+            $this->_em->persist($entity);
+            $this->_em->flush($entity);
+        }
+        return $entity;
+    }
+    
+    // /**
+    //  * @return CycleReportEditHistory[] Returns an array of CycleReportEditHistory objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?CycleReportEditHistory
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}

3e8b99e95e028765ceb38db894b83bd37fc398bd
Andrey Shamis <lolnik@gmail.com>
2021-08-22 14:02:22 +0300

    Fix table name for suite subs
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: Ie21397e8b50bbde54f8300577b188fa4bf51d76f

--- src/Entity/LogBookSuiteInfo.php
+++ src/Entity/LogBookSuiteInfo.php
@@ -50,7 +50,7 @@ class LogBookSuiteInfo
 
     /**
      * @ORM\ManyToMany(targetEntity=LogBookUser::class)
-     * @ORM\JoinTable(name="log_book_setup_log_book_user_fail_subscribers")
+     * @ORM\JoinTable(name="log_book_suite_log_book_user_fail_subscribers")
      */
     private $failureSubscribers;
 

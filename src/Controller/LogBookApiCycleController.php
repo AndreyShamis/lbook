8dae2e564b202b1629a0a36ac329bbeed0174419
Andrey Shamis <lolnik@gmail.com>
2021-08-29 14:50:04 +0300

    Close and send email for cycles
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I256fea966e9305c04d737b313eb760d7ae5c1d58

--- src/Controller/LogBookApiCycleController.php
+++ src/Controller/LogBookApiCycleController.php
@@ -3,26 +3,153 @@
 namespace App\Controller;
 
 use App\Entity\LogBookCycle;
+use App\Entity\LogBookCycleReport;
+use App\Entity\LogBookEmail;
 use App\Entity\LogBookSetup;
 use App\Entity\LogBookTest;
+use App\Entity\SuiteExecution;
 use App\Repository\LogBookCycleRepository;
+use App\Repository\LogBookDefectRepository;
 use App\Repository\LogBookSetupRepository;
 use App\Repository\LogBookTestRepository;
+use App\Repository\SuiteExecutionRepository;
 use App\Service\PagePaginator;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\ORM\Query;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 /**
  * Class LogBookApiCycleController
  * @package App\Controller
- * @Route("cycle/api/")
+ * @Route("api/cycle/")
  */
 class LogBookApiCycleController extends AbstractController
 {
+
+    /**
+     * @Route("auto/cycle_close", name="log_book_cycle_auto_close", methods={"GET","POST"})
+     * @param LogBookCycleRepository $cycleRepo
+     * @param LogBookTestRepository $testRepo
+     * @param SuiteExecutionRepository $suitesRepo
+     * @param LogBookDefectRepository $defectsRepo
+     * @param LoggerInterface $logger
+     * @return Response
+     */
+    public function auto_cycle_close(LogBookCycleRepository $cycleRepo, LogBookTestRepository $testRepo, SuiteExecutionRepository $suitesRepo, LoggerInterface $logger): Response
+    {
+        $time_limiter1 = new \DateTime('-90 minutes');
+        $time_limiter2 = new \DateTime('-40 hours');
+        $qb = $cycleRepo->createQueryBuilder('c')
+            ->where('c.timeEnd <= :time_limiter1')
+            ->andWhere('c.timeEnd > :time_limiter2')
+            ->andWhere('c.isClosed = :closed')
+            ->innerJoin('c.setup', 's') //->andWhere('s.autoCycleReport = 1')
+            ->setParameter('time_limiter1', $time_limiter1)
+            ->setParameter('time_limiter2', $time_limiter2)
+            ->setParameter('closed', false)
+//            ->setMaxResults(10)
+        ;
+
+        $cycles = $qb->getQuery()->execute();
+        $cycles_ret = [];
+        $i = 0;
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var LogBookCycle $cycle */
+        foreach ($cycles as $cycle) {
+            if ($cycle->isAllSuitesFinished() && count($cycle->getLogBookCycleReports()) <= 0) {
+                $l = $cycle->getTestingLevels();
+                if (count($l) == 1 && in_array($l[0], [ 'integration', 'nightly', 'weekly'])) {
+
+                    if ($i < 200) {
+                        $i += 1;
+                        $cycles_ret[] = $cycle;
+                        $cycle->close();
+                        $setup = $cycle->getSetup();
+                        try {
+                            $subscribers = $setup->getSubscribers();
+                            // $fail_subscribers = $setup->getFailureSubscribers();
+                            foreach ($subscribers as $subscriber) {
+                                if ( $subscriber->getEmail() === null) {
+                                    continue;
+                                }
+                                $newEmail = new LogBookEmail();
+                                $b = $cycle->getBuild();
+                                if ($cycle->getPassRate() < 100) {
+                                    $newEmail->setSubject('['. $cycle->getName() . ']['. $b . '] failed. PR:' . $cycle->getPassRate() . '%');
+                                } else{
+                                    $newEmail->setSubject('['. $cycle->getName() . ']['. $b . '] finished');
+                                }
+//                    try {
+//                        if ( $fail_subscribers->contains($subscriber) ) {
+//                            # For those who subscribed to failure (only)
+//                            continue;
+//                        }
+//                    } catch (\Throwable $ex) {}
+
+                                try {
+                                    $body = $this->get('twig')->render('lbook/email/cycle.finished.html.twig', [
+                                        'cycle' => $cycle,
+                                        'setup' => $setup
+                                    ]);
+                                    $newEmail->setBody($body);
+                                } catch (\Throwable $ex) {
+                                    $logger->critical($ex->getMessage());
+                                }
+
+                                $newEmail->setRecipient($subscriber);
+                                $em->persist($newEmail);
+                            }
+
+//                foreach ($fail_subscribers as $subscriber) {
+//                    if ( $subscriber->getEmail() === null) {
+//                        continue;
+//                    }
+//                    $newEmail = new LogBookEmail();
+//                    if ($cycle->getPassRate() < 100) {
+//                        $newEmail->setSubject('['. $cycle->getName() . '] failed. PR:' . $cycle->getPassRate() . '%');
+//                    } else{
+//                        continue;
+//                    }
+//                    try {
+//                        $body = $this->get('twig')->render('lbook/email/cycle.finished.html.twig', [
+//                            'cycle' => $cycle,
+//                            'setup' => $setup
+//                        ]);
+//                        $newEmail->setBody($body);
+//                    } catch (\Throwable $ex) {
+//                        $logger->critical($ex->getMessage());
+//                    }
+//
+//                    $newEmail->setRecipient($subscriber);
+//                    $em->persist($newEmail);
+//                }
+                        } catch (\Throwable $ex) {
+                            $logger->critical($ex->getMessage());
+                        }
+
+                    }
+
+
+                }
+            }
+            $cycle->setCalculateStatistic(false);
+        }
+
+        $em->flush();
+
+        return $this->render('lbook/cycle/index.html.twig', [
+            'size'      => count($cycles_ret),
+            'maxPages'  => 1,
+            'thisPage'  => 1,
+            'iterator'  => $cycles_ret
+        ]);
+    }
     /**
      * @Route("{cycle}", name="api_cycle_index")
      */

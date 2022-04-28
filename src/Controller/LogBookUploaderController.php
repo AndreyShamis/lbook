b5771a1618bbba62c506158c94eb3baa6594918b
Andrey Shamis <lolnik@gmail.com>
2022-04-28 10:14:34 +0300

    Improve parser
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I47996a1f2e089e72f226b6c1916b01ff063be0e3

--- src/Controller/LogBookUploaderController.php
+++ src/Controller/LogBookUploaderController.php
@@ -935,10 +935,17 @@ class LogBookUploaderController extends AbstractController
                         if ($t_fr_arr !== null && count($t_fr_arr) >= 2) {
                             $fail_reason = $t_fr_arr[0];
                         }
-                        if(strpos($fail_reason, "can't open file") != false && strpos($fail_reason, "[Errno 2]")) {
-                            $t_fr_arr = explode(" can't open file ", $fail_reason);
-                            $t_fr_arr2 = explode(" [Errno 2] ", $fail_reason);
-                            $fail_reason = $t_fr_arr[0] . " can't open file" . ". [Errno 2] " . $t_fr_arr2[1];
+                        if(strpos($fail_reason, "can't open file") !== false) {
+
+                            $t_fr_arr = explode("can't open file", $fail_reason);
+                        
+                            if ($t_fr_arr !== null && count($t_fr_arr) >= 2) {
+                                if (strpos($t_fr_arr[1], "[Errno 2]") !== false) {
+                                    $t_fr_arr2 = explode("[Errno 2]", $fail_reason);
+                                    $fail_reason = $t_fr_arr[0] . "can't open file" . ".[Errno 2]" . $t_fr_arr2[1];
+                                }
+                            }
+                        
                         }
                     } catch (\Throwable $ex) {
                         $logger->alert('Failed to clean fail_reason :' . $ex->getMessage(), $ex->getTrace());
